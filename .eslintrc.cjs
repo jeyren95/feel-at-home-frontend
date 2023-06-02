0b5401b5ec46a5fb2279b764f2d322d732696269
Poon Jey-ren <jeyren95@hotmail.com>
2023-06-02 17:30:02 +0800

    Added eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,29 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-}
+  plugins: [
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+  },
+};
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react';
+
+const DashboardPage = (): ReactElement => <div>Dashboard page</div>;
+
+export default DashboardPage;
